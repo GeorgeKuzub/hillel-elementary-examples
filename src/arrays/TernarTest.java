adc6df07ded6b01db93f12313d1dd38068d348cc
GeoKuzub <geo.kuzub@gmail.com>
2017-08-04 19:50:59 +0300

    Ternar operation with meth

--- src/arrays/TernarTest.java
+++ src/arrays/TernarTest.java
@@ -7,12 +7,19 @@ public class TernarTest {
 
     public static void main(String[] args) {
         double d1 = 0.5;
-        double d2 = 0.5;
+        double d2 = 0.45;
         System.out.println(getDoAction(d1, d2));
     }
 
-    private static String getDoAction(double d1, double d2) {
-        return (d1 >= d2 ? "d1 >= d2" : "d1 < d2");
+    private static String meth1() {
+        return "d1 >= d2";
+    }
 
+    private static String meth2() {
+        return " d1 < d2";
+    }
+
+    private static String getDoAction(double d1, double d2) {
+        return (d1 >= d2 ? meth1() : meth2());
     }
 }
