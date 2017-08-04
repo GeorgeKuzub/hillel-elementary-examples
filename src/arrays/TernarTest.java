f5f7e95422628336f02e8aab22c7583ae2bc9e0a
GeoKuzub <geo.kuzub@gmail.com>
2017-08-04 19:47:42 +0300

    Ternar operation

---
+++ src/arrays/TernarTest.java
@@ -0,0 +1,18 @@
+package arrays;
+
+/**
+ * Created by User on 04.08.2017.
+ */
+public class TernarTest {
+
+    public static void main(String[] args) {
+        double d1 = 0.5;
+        double d2 = 0.5;
+        System.out.println(getDoAction(d1, d2));
+    }
+
+    private static String getDoAction(double d1, double d2) {
+        return (d1 >= d2 ? "d1 >= d2" : "d1 < d2");
+
+    }
+}
