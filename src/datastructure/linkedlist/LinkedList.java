aade36e3faacb928ab5757bcac8a449b12d0fbbc
GeoKuzub <geo.kuzub@gmail.com>
2017-09-15 21:21:24 +0300

    Double Linked List

---
+++ src/datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,92 @@
+package datastructure.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node find(String data) {
+        Node currentNode = head;
+
+        {
+            if (currentNode.getData().equals(data)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        } while (currentNode != null);
+
+        return null;
+
+    }
+
+    public boolean contain(String data) {
+        return (find(data) != null);
+    }
+
+
+    public void insertBefore(String data) {
+        Node currentNode = new Node(data);
+        head.setPrev(currentNode);
+        currentNode.setNext(head);
+        head = currentNode;
+    }
+
+    public void insertAfter(String data) {
+        Node newlyAddedNode = new Node(data);
+
+        if (head == null) {
+            head = newlyAddedNode;
+            tail = head;
+            return;
+        }
+
+
+        newlyAddedNode.setPrev(tail);
+        tail.setNext(newlyAddedNode);
+
+        tail = newlyAddedNode;
+
+
+    }
+
+    public void remove(String data) {
+        Node removingNode = find(data);
+        remove(removingNode);
+
+    }
+
+    public void remove(Node removingNode) {
+        if (removingNode == null) {
+            System.out.println("Warning: Removing element has not been found.");
+            return;
+        }
+
+        if (removingNode == head) {
+            Node currentNode = head.getNext();
+            head.setNext(null);
+            head = currentNode;
+            currentNode.setPrev(null);
+            return;
+        }
+
+        if (removingNode == tail) {
+            Node currentNode = tail.getPrev();
+            tail.setPrev(null);
+            tail = currentNode;
+            currentNode.setNext(null);
+            return;
+        }
+
+        Node leftNode = removingNode.getPrev();
+        Node rightNode = removingNode.getNext();
+        removingNode.setPrev(null);
+        removingNode.setNext(null);
+        leftNode.setNext(rightNode);
+        rightNode.setPrev(leftNode);
+
+    }
+
+}
---
+++ src/datastructure/linkedlist/Start.java
@@ -0,0 +1,35 @@
+package datastructure.linkedlist;
+
+import sun.awt.image.ImageWatched;
+
+public class Start {
+
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertAfter("aa");
+        linkedList.insertAfter("bb");
+        linkedList.insertAfter("cc");
+        linkedList.insertAfter("dd");
+        linkedList.insertAfter("ee");
+
+        showLinkedList(linkedList);
+
+        linkedList.remove("ee");
+        showLinkedList(linkedList);
+
+        linkedList.remove("aa");
+        showLinkedList(linkedList);
+
+        linkedList.remove("cc");
+        showLinkedList(linkedList);
+    }
+
+    public static void showLinkedList(LinkedList linkedList) {
+        Node currentNode = linkedList.getHead();
+        while (currentNode != null) {
+            System.out.println(currentNode.getData());
+            currentNode = currentNode.getNext();
+        }
+    }
+}
