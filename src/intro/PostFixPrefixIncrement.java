f45b262695144feb70440c25054503dd4f1daa2a
Yurik81 <yura.kuzub@gmail.com>
2018-02-09 16:08:08 +0200

    Revert "Insert wrong code"
    This reverts commit 1d2e3a3

--- src/intro/PostFixPrefixIncrement.java
+++ src/intro/PostFixPrefixIncrement.java
@@ -6,11 +6,9 @@ public class PostFixPrefixIncrement {
 
     public static void main(String[] args) {
         int i = 4;
-        int k = i++ * i++;
+        int k = i++ * ++i;
 
         System.out.println("i=" + i);
         System.out.println("k=" + k);
-
-        // wrong code
     }
 }
